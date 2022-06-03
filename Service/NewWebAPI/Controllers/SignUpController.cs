4294a0f4cbd835d1476569490d96f692cc8c64b0
Jian Xue <royjianxue@yahoo.com>
2022-06-02 20:51:44 -0400

    Moved database Code into DatabaseManager Class from Controller class.

--- Service/NewWebAPI/Controllers/SignUpController.cs
+++ Service/NewWebAPI/Controllers/SignUpController.cs
@@ -11,14 +11,14 @@ namespace NewWebAPI.Controllers
         [HttpGet]
         public List<Participant> GetALLParticipants()
         {
-            return LoadParticipantsFromDB();
+            return DatabaseManager.LoadParticipantsFromDB();
         }
 
         [HttpGet]
         [Route("GetByID/{participantid}")]
         public List<Participant>GetParticipantByID(int participantId)
         {
-            return LoadParticipantsFromDB().Where(p => p.Id == participantId).ToList();
+            return DatabaseManager.LoadParticipantsFromDB().Where(p => p.Id == participantId).ToList();
         }
 
         [HttpPost]
@@ -26,36 +26,15 @@ namespace NewWebAPI.Controllers
         public string PostParticipants(Participant participant)
         {
 
-            using (var conn = DatabaseManager.CreatConnection())
-            {
-                using (var cmd = conn.CreateCommand())
-                {
-                    cmd.CommandText = $@"USE campformDB INSERT INTO SignUpData (FirstName, LastName, IsFirstTime) Values(@FirstName, @LastName, @IsFirstTime)";
-
-                    cmd.Parameters.AddWithValue("@FirstName", participant.FirstName);
-                    cmd.Parameters.AddWithValue("@LastName", participant.LastName);
-                    cmd.Parameters.AddWithValue("@IsFirstTime", participant.IsFirstTime);
-
-                    cmd.ExecuteNonQuery();
-                }
-            }
-            return "Participant Added Successfully";
+            return DatabaseManager.InsertData(participant);
+ 
         }
 
         [HttpDelete]
         [Route("DeleteAll")]
         public string DeleteAll()
-        {
-            using (var conn = DatabaseManager.CreatConnection())
-            {
-                using (var cmd = conn.CreateCommand())
-                {
-
-                    cmd.CommandText = $"USE campformDB DELETE FROM SignUpData";
-                    cmd.ExecuteNonQuery();
-                }
-            }
-            return "Participants Deleted Successfully";
+        {           
+            return DatabaseManager.DeleteAllData();     
         }
 
 
@@ -63,80 +42,16 @@ namespace NewWebAPI.Controllers
         [Route("Delete/{participantid}")]
         public string DeleteParticipants(int participantId)
         {
-            var checkDatabase = LoadParticipantsFromDB().Where(p => p.Id == participantId).ToList();
-
-            if (checkDatabase.Count() == 0)
-            {
-                return "Id Not Found";
-            }
-            using (var conn = DatabaseManager.CreatConnection())
-            {
-                using (var cmd = conn.CreateCommand())
-                {
-
-                    cmd.CommandText = $"USE campformDB DELETE FROM SignUpData WHERE Id =" + participantId + "";
-                    cmd.ExecuteNonQuery();
-                }
-            }
-            return "Participant Deleted Successfully";
+            return DatabaseManager.DeleteById(participantId);
         }
 
         [HttpPut]
         [Route("Update/{participantid}")]
         public string UpdateParticipants(int participantId, Participant participant)
         {
-            var checkDatabase = LoadParticipantsFromDB().Where(P => P.Id == participantId).ToList();
-
-            if (checkDatabase.Count() == 0)
-            {
-                return "Id Not Found";
-            }
-            using (var conn = DatabaseManager.CreatConnection())
-            {
-                using (var cmd = conn.CreateCommand())
-                {
-
-                    cmd.CommandText = $"USE campformDB UPDATE SignUpData SET FirstName = @FirstName, LastName = @LastName, IsFirstTime = @IsFirstTime  WHERE Id = {participantId}";
-                    cmd.Parameters.AddWithValue("@FirstName", participant.FirstName);
-                    cmd.Parameters.AddWithValue("@LastName", participant.LastName);
-                    cmd.Parameters.AddWithValue("@IsFirstTime", participant.IsFirstTime);
-
-                    cmd.ExecuteNonQuery();
-                }
-            }
-            return "Participant Updated Successfully";
+            return DatabaseManager.UpdateData(participantId, participant);  
         }
 
-        private List<Participant> LoadParticipantsFromDB()
-        {
-            List<Participant> participants = new List<Participant>();
-
-            using (var conn = DatabaseManager.CreatConnection())
-            {
-                using (var cmd = conn.CreateCommand())
-                {
-                    cmd.CommandText = $" USE campformDB SELECT * FROM SignUpData";
-
-                    var rdr = cmd.ExecuteReader(); 
-
-                    while(rdr.Read())
-                    {
-                        int id = rdr.GetInt32(0);
-                        string firstName = rdr.GetString(1);
-                        string lastName = rdr.GetString(2);
-                        bool isFirstTime = rdr.GetBoolean(3);
-
-                        participants.Add(new Participant()
-                        {
-                            Id = id,
-                            FirstName = firstName,
-                            LastName = lastName,
-                            IsFirstTime = isFirstTime
-                        });
-                    }
-                }
-            }
-            return participants;
-        }
+       
     }
 }
