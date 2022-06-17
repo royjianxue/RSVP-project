7edcc1fde3122afce68590ab0defd7a725724d00
Jian Xue <royjianxue@yahoo.com>
2022-06-17 01:10:42 -0400

    refine appsettings.json

--- Service/ASP.NET.CORE.WEBAPI/Program.cs
+++ Service/ASP.NET.CORE.WEBAPI/Program.cs
@@ -1,8 +1,6 @@
 using BusinessProviders.Business;
 using DataProviders.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using System.Diagnostics;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
